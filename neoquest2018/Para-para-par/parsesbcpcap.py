4a315147ab7d3e5b1eb0944b0aab2924800b0db6
AetherEternity <swing056@gmail.com>
2018-08-29 12:22:32 +0300

    Added neoquest bluetooth task

---
+++ neoquest2018/Para-para-par/parsesbcpcap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+filesize=0x6d8c9+1
+pcapInitSpacing=0x2b+1
+pcapDataSpacing=0x13+1
+frameHeader=0x15 
+packetHeader=0x4
+packetSize=0x4e+1
+f=open('sbconly.pcap','rb')
+o=open('flag.sbc','wb')
+
+ptr=pcapInitSpacing
+while ptr<filesize:
+	ptr+=frameHeader
+	f.seek(ptr)
+	cnt=int.from_bytes(f.read(1),'big')
+	ptr+=1
+#	print(hex(cnt)+'at'+hex(ptr)+'\n')
+	f.seek(ptr)	
+	o.write(f.read((packetSize+packetHeader)*cnt))
+	ptr+=(packetSize+packetHeader)*cnt+pcapDataSpacing
+f.close()
+o.close()
---
+++ neoquest2018/README.md
@@ -0,0 +1,7 @@
+# NeoQuest 2018 
+
+CTF took place at [neoquest.ru](https://neoquest.ru/2018/). 
+
+# Tasks: 
+
+[Пара-пара-пар!](./Para-para-par) 
